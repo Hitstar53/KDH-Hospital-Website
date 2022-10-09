80ba3b6270b3e9b18730768a0812f0759062f0db
Hatim Sawai <hatimsawai76@gmail.com>
2022-10-09 12:07:55 +0530

    minor edit
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 $(document).ready(function(){
-
   $('.fa-bars').click(function(){
     $(this).toggleClass('fa-times');
     $('.nav').toggleClass('nav-toggle');
   });
-
   $(window).on('load scroll',function(){
-
     $('.fa-bars').removeClass('fa-times');
     $('.nav').removeClass('nav-toggle');
-
     if($(window).scrollTop() > 10){
       $('header').addClass('header-active');
     }else{
       $('header').removeClass('header-active');
     }
-
   });
-
   $('.facility').magnificPopup({
     delegate:'a',
     type:'image',
@@ -25,5 +19,4 @@ $(document).ready(function(){
       enabled:true
     }
   });
-
 });
