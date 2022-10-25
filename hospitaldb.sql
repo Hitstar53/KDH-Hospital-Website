0cff998a79349ebbbe2306718dd7bd9362d38057
Hatim Sawai <hatimsawai76@gmail.com>
2022-10-25 23:38:35 +0530

    readying for deployment
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- hospitaldb.sql
+++
@@ -1,6 +0,0 @@
--- @BLOCK
-INSERT INTO doctors VALUES(1, 'Dr. John Doe', 'Cardiology', '1234567890', 'john@gmail.com', '12345678');
--- @BLOCK
-DROP TABLE IF EXISTS appointments;
--- @BLOCK
-DELETE FROM appointments WHERE id >= 1;
