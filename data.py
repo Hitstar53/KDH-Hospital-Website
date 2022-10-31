bdae8c074232eaf57f2a8e3c68073f23a50da36c
Hatim Sawai <hatimsawai76@gmail.com>
2022-10-31 22:52:39 +0530

    fixed bugs and updated database
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- data.py
+++ data.py
@@ -5,18 +5,57 @@ import sqlite3
 conn = sqlite3.connect('instance/database.db')
 cursor = conn.cursor()
 
-sql = "INSERT INTO doctors VALUES(1,'John Doe',35,1234567890,'johndoe@gmail.com','cardiology')"
+sql = "INSERT INTO doctors VALUES(1,'John Doe',25,9898785357,'johndoe@gmail.com','cardiology')"
 cursor.execute(sql)
 conn.commit()
 
-sql = "INSERT INTO doctors VALUES(2,'John2 Doe',35,1234567890,'john2doe@gmail.com','neurology')"
+sql = "INSERT INTO doctors VALUES(2,'Thomas Shelby',45,7896545665,'thomasshelby@gmail.com','neurology')"
 cursor.execute(sql)
 conn.commit()
 
-sql = "INSERT INTO doctors VALUES(3,'John3 Doe',35,1234567890,'john3doe@gmail.com','dermatology')"
+sql = "INSERT INTO doctors VALUES(3,'Michael Gray',20,8999876545,'michaelgray@gmail.com','dermatology')"
 cursor.execute(sql)
 conn.commit()
 
+sql = "INSERT INTO doctors VALUES(4,'Ada Thorne',28,98786545687,'adathorne@gmail.com','Opthalmology')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(5,'Arthur Shelby',50,9999988875,'arthurshelby@gmail.com','Dermatolgy')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(6,'Charlie Hourse',55,9887889876,'charliehorses@gmail.com','Pathology')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(7,'Billy Kimber',44,9877896540,'billykimber@gmail.com','Orthopedics')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(8,'Alfie Solomon',60,8798797895,'alfiesolomons@gmail.com','Surgery')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(9,'Polly Blackmagic',65,9788856786,'pollyblackmagic@gmail.com','Radiology')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(10,'Mathew Modine',35,1234567890,'mathewmodine@gmail.com','Dentistry')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(11,'Grace Locke',25,7777778889,'gracelocke@gmail.com','cardiology')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(12,'Lucas Sinclair',25,7777778889,'lucassinclair@gmail.com','cardiology')"
+cursor.execute(sql)
+conn.commit()
+
+sql = "INSERT INTO doctors VALUES(13,'Antonio Zuckerberg',25,7777778889,'antonioberg@gmail.com','cardiology')"
+cursor.execute(sql)
+conn.commit()
 
 #method 2:
 '''from werkzeug.security import generate_password_hash,check_password_hash
