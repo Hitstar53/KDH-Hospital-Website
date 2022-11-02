db1e855e7a9676c7052c8eea46d2f0c666eb8e61
Hatim Sawai <hatimsawai76@gmail.com>
2022-11-02 09:48:51 +0530

    fixes
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- website/__init__.py
+++ website/__init__.py
@@ -17,6 +17,7 @@ def create_app():
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:hospital123@hospital.c36zbkr1bobf.ap-south-1.rds.amazonaws.com:3306/hospital'
     #new db - freesqldatabase
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql12533559:bum3pMdUv1@sql12.freesqldatabase.com:3306/sql12533559'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Hitstar53:hospital123@Hitstar53.mysql.pythonanywhere-services.com:3306/Hitstar53$hospital'
     db.init_app(app)
 
     from .views import views
