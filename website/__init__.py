6e40b89ff2f9e87b41d018af97eefa235163b45d
Hatim Sawai <hatimsawai76@gmail.com>
2022-11-04 20:07:25 +0530

    fixed ui styling
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- website/__init__.py
+++ website/__init__.py
@@ -10,13 +10,13 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'kdh5253'
     #old db - sqLite
-    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     #new db - mysql localhost
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sqluser:password@localhost/hospital'
     #new db - free awd
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:hospital123@hospital.c36zbkr1bobf.ap-south-1.rds.amazonaws.com:3306/hospital'
     #new db - freesqldatabase
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql12533559:bum3pMdUv1@sql12.freesqldatabase.com:3306/sql12533559'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql12533559:bum3pMdUv1@sql12.freesqldatabase.com:3306/sql12533559'
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Hitstar53:hospital123@Hitstar53.mysql.pythonanywhere-services.com:3306/Hitstar53$hospital'
     db.init_app(app)
 
